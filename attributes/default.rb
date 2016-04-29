0e2fea500e37c9ecb073f4c338755252844d0e8e
John Snow <thelunaticscripter@outlook.com>
2016-04-29 15:41:53 -0400

    Corrected conflicts with github push

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
 default['ls_windows_dns']['dns_servers'] = "'10.0.3.248','10.0.3.10','10.0.4.10'"
-=======
-default['ls_windows_dns']['dns_server1'] = '10.0.3.2'
-default['ls_windows_dns']['dns_server2'] = '10.0.3.3'
-default['ls_windows_dns']['dns_server3'] = '10.0.3.4'
-default['ls_windows_dns']['dns_server4'] = '10.0.4.5'
->>>>>>> feb0a8ecb5c123a0deae8527541c97c1fed3ecd2
+
