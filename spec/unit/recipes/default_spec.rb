52c958cd381cb0b50f119db66cd96659b7530685
John Snow <jsnow@chef.io>
2017-03-01 08:52:24 -0500

    Fix unit test so stub command is not a real command but just a string

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -14,14 +14,7 @@ describe 'ls_windows_dns::default' do
     end
 
     before do
-      script = "
-        try{
-            $NIC = Get-NetAdapter | where {$_.Status -eq \"Up\"}
-            $dnsServers = $NIC | Get-DnsClientServerAddress -AddressFamily IPv4
-            $CorrectDNSServers = \"\'10.0.0.1\',10.0.0.2\"
-        }
-        catch{}
-        (Compare-Object $dnsServers.ServerAddresses $CorrectDNSServers -sync 0).Length -eq 0"
+      script = '(Compare-Object $dnsServers.ServerAddresses $CorrectDNSServers -sync 0).Length -eq 0'
       stub_command(script).and_return(true)
     end
 
